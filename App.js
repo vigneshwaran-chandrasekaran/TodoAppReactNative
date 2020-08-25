ad0413cb077685d459cf76cf6c51308bc2148ea6
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-08-25 22:42:36 +0530

    Login screen created

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Vigneshwaran</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<LoginScreen />
+			<View style={styles.container}>
+				<Text>Vigneshwaran</Text>
+				<StatusBar style="auto" />
+			</View>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
