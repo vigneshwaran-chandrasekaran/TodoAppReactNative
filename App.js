70c0c0f962c9d06570f34a1df6b7b1ba7e7a29d7
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-08-26 20:41:48 +0530

    no needed app.js file code removed

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
 	return (
 		<>
 			<LoginScreen />
-			<View style={styles.container}>
-				<Text>Vigneshwaran</Text>
-				<StatusBar style="auto" />
-			</View>
 		</>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
